52e16b036cd8e6e29b03a9b95ecd0fda102260c6
natanieljr <Nataniel.123>
2017-05-20 16:05:05 +0200

    Removed unused methods. Added reboot and unlock

--- src/main/java/org/droidmate/analyzer/wrappers/AdbConsts.java
+++ src/main/java/org/droidmate/analyzer/wrappers/AdbConsts.java
@@ -4,9 +4,10 @@ package org.droidmate.analyzer.wrappers;
  * Constants to perform actions using ADB command line tool
  */
 class AdbConsts {
-    static String INSTALL = "adb install";
-    static String UNINSTALL = "adb uninstall";
-    static String PULL = "adb pull";
-    static String RUN_INTENT = "adb shell am start";
-    static String REMOVE = "adb shell rm";
+    static final String REBOOT = "adb reboot";
+    static final String UNLOCK = "adb shell input keyevent 82";
+    // Returns Parcel(00000000 00000000) if locked and Parcel(00000000 00000001) if unlocked
+    static final String CHECK_LOCKED = "adb shell service call power 12";
+
+    static final int REBOOT_TIME = 60 * 1000; // ms
 }
--- src/main/java/org/droidmate/analyzer/wrappers/AdbWrapper.java
+++ src/main/java/org/droidmate/analyzer/wrappers/AdbWrapper.java
@@ -1,112 +1,57 @@
 package org.droidmate.analyzer.wrappers;
 
-import org.droidmate.analyzer.IAppUnderTest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * Wrapper to the ADB command line tool
  */
-public class AdbWrapper {
+class AdbWrapper {
     private static final Logger logger = LoggerFactory.getLogger(AdbWrapper.class);
 
-    public int installApk(IAppUnderTest apk) {
-        logger.info(String.format("ADB INSTALL: %s", apk.getApkFile().getFileName()));
-
-        String filePath = apk.getApkFile().toString();
-        CommandLineWrapper exec = new CommandLineWrapper();
-
+    private int reboot(){
+        logger.info("Rebooting device before exploration");
         try {
-            String[] output = exec.execute(AdbConsts.INSTALL, filePath);
-            return CommandLineWrapper.evalOutput(output);
-        } catch (IOException e) {
-            logger.error(e.getMessage(), e);
-        }
-
-        return 1;
-    }
-
-    public int uninstallApk(IAppUnderTest apk) {
-        String packageName = apk.getPackageName();
-        logger.info(String.format("ADB UNINSTALL: %s", packageName));
-
-        CommandLineWrapper exec = new CommandLineWrapper();
-        try {
-            String[] output = exec.execute(AdbConsts.UNINSTALL, packageName);
-            return CommandLineWrapper.evalOutput(output);
-        } catch (IOException e) {
-            logger.error(e.getMessage(), e);
-        }
-
-        return 1;
-    }
-
-    public int pull(Path srcFile, Path dstFile) {
-        // Convert from Windows do Linux path if necessary
-        String src = srcFile.toString().replace('\\', '/');
-        String dst = dstFile.toAbsolutePath().toString();
-
-        logger.info(String.format("ADB PULL: %s %s", src, dst));
-
-        try {
-            // Create dir if not exists
-            Path dstDir = dstFile.getParent();
-            if (!Files.exists(dstDir))
-                Files.createDirectories(dstDir);
-
-            // Remove old file if necessary
-            Files.deleteIfExists(dstFile);
-
             CommandLineWrapper exec = new CommandLineWrapper();
-            String[] output = exec.execute(AdbConsts.PULL,
-                    src, dst);
+            String[] output = exec.execute(AdbConsts.REBOOT);
+
             return CommandLineWrapper.evalOutput(output);
-        } catch (IOException e) {
+        } catch (Exception e) {
             logger.error(e.getMessage(), e);
         }
 
-        return 1;
+        return -1;
     }
 
-    public int remove(Path srcFile) {
-        // Convert from Windows do Linux path if necessary
-        String src = srcFile.toString().replace('\\', '/');
-
-        logger.info(String.format("ADB REMOVE: %s", src));
-
-        CommandLineWrapper exec = new CommandLineWrapper();
+    private int unlock(){
+        logger.info("Unlocking device screen");
         try {
-            String[] output = exec.execute(AdbConsts.REMOVE, src);
+            CommandLineWrapper exec = new CommandLineWrapper();
+            String[] output = exec.execute(AdbConsts.UNLOCK);
+
             return CommandLineWrapper.evalOutput(output);
         } catch (IOException e) {
             logger.error(e.getMessage(), e);
         }
 
-        return 1;
+        return -1;
     }
 
-    public int runIntent(String intent, String... params) {
-        logger.info(String.format("ADB SHELL START: %s", intent));
+    public void rebootAndUnlock(){
+        int res = this.reboot();
+        assert res == 0;
 
-        CommandLineWrapper exec = new CommandLineWrapper();
-        List<String> command = new ArrayList<>();
-        command.addAll(Arrays.asList(AdbConsts.RUN_INTENT.split(" ")));
-        command.addAll(Arrays.asList(intent.split(" ")));
-        command.addAll(Arrays.asList(params));
         try {
-            String[] output = exec.execute((String[]) command.toArray());
-            return CommandLineWrapper.evalOutput(output);
-        } catch (IOException e) {
+            logger.debug(String.format("Waiting %d seconds for reboot", AdbConsts.REBOOT_TIME));
+            Thread.sleep(AdbConsts.REBOOT_TIME);
+        }
+        catch(InterruptedException e){
             logger.error(e.getMessage(), e);
         }
 
-        return 1;
+        res = this.unlock();
+        assert res == 0;
     }
 }
