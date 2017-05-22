74913b343e138b557f910f2c24b854245909002d
natanieljr <Nataniel.123>
2017-05-22 16:48:04 +0200

    Set experiment time to 15min

--- src/main/java/org/droidmate/analyzer/wrappers/BoxMateConsts.java
+++ src/main/java/org/droidmate/analyzer/wrappers/BoxMateConsts.java
@@ -8,7 +8,7 @@ class BoxMateConsts {
     static final String ARGS_INLINE = "-inline";
     static final String ARGS_API23 = "-apiLevel=23";
     static final String ARGS_RESET = "-resetEvery=30";
-    static final String ARGS_TIME = "-timeLimit=30";
+    static final String ARGS_TIME = "-timeLimit=600";
     static final String ARGS_SEED = "-randomSeed=0";
     static final String ARGS_SNAP = "-getValidGuiSnapshotRetryAttempts=2";
     static final String ARGS_UNPACK = "-unpack";
