75bd68a26411d81c6b3bebd089d9e7b5c929bba8
natanieljr <Nataniel.123>
2017-05-26 15:21:58 +0200

    Set exploration time to 15min

--- src/main/java/org/droidmate/analyzer/wrappers/BoxMateConsts.java
+++ src/main/java/org/droidmate/analyzer/wrappers/BoxMateConsts.java
@@ -12,7 +12,7 @@ public class BoxMateConsts {
     static final String ARGS_INLINE = "-inline";
     static final String ARGS_API23 = "-apiLevel=23";
     static final String ARGS_RESET = "-resetEvery=30";
-    static final String ARGS_TIME = "-timeLimit=90";//0";
+    static final String ARGS_TIME = "-timeLimit=900";
     static final String ARGS_SEED = "-randomSeed=0";
     static final String ARGS_SNAP = "-getValidGuiSnapshotRetryAttempts=2";
     static final String ARGS_REPLACE_RESOURCES = "-replaceExtractedResources=false";
